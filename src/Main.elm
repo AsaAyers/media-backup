bf0e08dd631588ee171bbafbf319e3acc0797e0f
Asa Ayers <Asa@AsaAyers.com>
2017-09-04 12:59:56 -0700

    Reset storage when initialiation fails

--- src/Main.elm
+++ src/Main.elm
@@ -13,43 +13,59 @@ main =
         , view = view
         }
 
+
 init : Maybe Model -> ( Model, Cmd msg )
 init model =
     case model of
         Just model ->
             ( model, Cmd.none )
+
         Nothing ->
             ( defaultModel, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+
 port setStorage : Model -> Cmd msg
 
-defaultModel: number
-defaultModel = 0
-type alias Model = Int
 
-type Msg = Increment | Decrement
+defaultModel : { counter : Int }
+defaultModel =
+    { counter = 0
+    }
+
+
+type alias Model =
+    { counter : Int
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  let
-    newModel =
-      case msg of
-        Increment ->
-          model + 1
+    let
+        newModel =
+            case msg of
+                Increment ->
+                    { model | counter = model.counter + 1 }
 
-        Decrement ->
-          model - 1
-      in
-        (newModel, setStorage newModel)
+                Decrement ->
+                    { model | counter = model.counter - 1 }
+    in
+        ( newModel, setStorage newModel )
+
 
 view : model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
--- src/ports.js
+++ src/ports.js
@@ -4,8 +4,16 @@ const container = document.getElementById('app');
 const model = localStorage.getItem('model');
 const startingModel = model ? JSON.parse(model) : null;
 
-const app = Elm.Main.embed(container, startingModel);
+function boot() {
+  try {
+    const app = Elm.Main.embed(container, startingModel);
+    app.ports.setStorage.subscribe((state) => {
+      localStorage.setItem('model', JSON.stringify(state));
+    });
+  } catch (e) {
+    localStorage.removeItem('model');
+    throw new Error('Your model has been reset');
+  }
+}
 
-app.ports.setStorage.subscribe((state) => {
-  localStorage.setItem('model', JSON.stringify(state));
-});
+boot();
