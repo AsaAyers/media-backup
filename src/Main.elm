84ddcebdc48dd2e6956006f10af9d84b2282f33b
Asa Ayers <Asa@AsaAyers.com>
2017-09-04 12:59:34 -0700

    Save the model to localStorage and restore it on refresh

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,55 @@
-import Html
+port module Main exposing (..)
 
-main = Html.text "hello Elm!"
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+main : Program (Maybe Model) Model Msg
+main =
+    Html.programWithFlags
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+init : Maybe Model -> ( Model, Cmd msg )
+init model =
+    case model of
+        Just model ->
+            ( model, Cmd.none )
+        Nothing ->
+            ( defaultModel, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+port setStorage : Model -> Cmd msg
+
+defaultModel: number
+defaultModel = 0
+type alias Model = Int
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  let
+    newModel =
+      case msg of
+        Increment ->
+          model + 1
+
+        Decrement ->
+          model - 1
+      in
+        (newModel, setStorage newModel)
+
+view : model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
--- src/ports.js
+++ src/ports.js
@@ -1,6 +1,11 @@
-
 const Elm = require('./Main.elm');
 
 const container = document.getElementById('app');
+const model = localStorage.getItem('model');
+const startingModel = model ? JSON.parse(model) : null;
 
-const app = Elm.Main.embed(container);
+const app = Elm.Main.embed(container, startingModel);
+
+app.ports.setStorage.subscribe((state) => {
+  localStorage.setItem('model', JSON.stringify(state));
+});
